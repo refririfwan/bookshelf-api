1ff7ad5d924b901a9de2ac5fc6c141fdc5625b73
Refri Rifwan <refririfwan@gmail.com>
2021-04-19 00:34:10 +0700

    Kriteria 1 : API dapat menyimpan buku

---
+++ src/handler.js
@@ -0,0 +1,73 @@
+const { nanoid } = require('nanoid');
+const books = require('./books');
+
+const createBookHandler = (req, h) => {
+  const data = JSON.parse(req.payload);
+
+  const {
+    name, year, author, summary, publisher, pageCount, readPage, reading,
+  } = data;
+
+  const id = nanoid(16);
+  const finished = false;
+  const createdAt = new Date().toISOString();
+  const updatedAt = createdAt;
+
+  const newBook = {
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+    id,
+    finished,
+    createdAt,
+    updatedAt,
+  };
+
+  books.push(newBook);
+
+  const isSuccess = books.filter((book) => book.id === id).length > 0;
+
+  if (!name) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. Mohon isi nama buku',
+    });
+    response.code(400);
+    return response;
+  }
+
+  if (readPage > pageCount) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
+    });
+    response.code(400);
+    return response;
+  }
+
+  if (isSuccess) {
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil ditambahkan',
+      data: {
+        bookId: id,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan gagal ditambahkan',
+  });
+  response.code(500);
+  return response;
+};
+
+module.exports = { createBookHandler };
